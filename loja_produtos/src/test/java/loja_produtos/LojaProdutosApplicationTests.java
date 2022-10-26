4d3f4d7a40d5f3942231916c05666b935b0f24e4
angra.souza <agra.santos@hotmail.com.br>
2022-10-25 23:23:18 -0300

    first commit

---
+++ loja_produtos/src/test/java/loja_produtos/LojaProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package loja_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
